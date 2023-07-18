97c048c83a0d76794f6b9fcd5b94a51e22c0d6c9
Ahmet <130960696+AhmetIclier@users.noreply.github.com>
2023-07-18 15:15:00 +0200

    added level structure

--- classes/world.class.js
+++ classes/world.class.js
@@ -1,41 +1,12 @@
 class World {
     character = new Character();
+    enemies = level1.enemies;
+    clouds = level1.clouds;
+    background = level1.backgroundObjects;
     canvas;
     ctx;
     keyboard;
     camera_x = 0;
-
-    enemies = [
-        new Chicken(),
-        new Chicken(),
-        new Chicken(),
-    ];
-
-    clouds = [
-        new Cloud(),
-    ]
-
-    background = [
-        new Background('img/5_background/layers/air.png', 480, 0, -719),
-        new Background('img/5_background/layers/3_third_layer/2.png', 400, 80, -719),
-        new Background('img/5_background/layers/2_second_layer/2.png', 400, 80, -719),
-        new Background('img/5_background/layers/1_first_layer/2.png', 400, 80, -719),
-
-        new Background('img/5_background/layers/air.png', 480, 0, 0),
-        new Background('img/5_background/layers/3_third_layer/1.png', 400, 80, 0),
-        new Background('img/5_background/layers/2_second_layer/1.png', 400, 80, 0),
-        new Background('img/5_background/layers/1_first_layer/1.png', 400, 80, 0),
-
-        new Background('img/5_background/layers/air.png', 480, 0, 719),
-        new Background('img/5_background/layers/3_third_layer/2.png', 400, 80, 719),
-        new Background('img/5_background/layers/2_second_layer/2.png', 400, 80, 719),
-        new Background('img/5_background/layers/1_first_layer/2.png', 400, 80, 719),
-        
-        new Background('img/5_background/layers/air.png', 480, 0, 1438),
-        new Background('img/5_background/layers/3_third_layer/1.png', 400, 80, 1438),
-        new Background('img/5_background/layers/2_second_layer/1.png', 400, 80, 1438),
-        new Background('img/5_background/layers/1_first_layer/1.png', 400, 80, 1438)
-    ]
 
 
     constructor(canvas, keyboard) {
